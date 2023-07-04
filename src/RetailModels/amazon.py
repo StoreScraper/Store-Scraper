0de73e8311cd08b1e189db9bbeceb9e3be717d7e
Isaac Tapia <isaac.141294@gmail.com>
2023-07-03 20:18:42 -0700

    Delete src directory

--- src/RetailModels/amazon.py
+++
@@ -1,9 +0,0 @@
-from Base import Retailer
-
-class Amazon(Retailer):
-    def __init__(self) -> None:
-        self.name = "Amazon"
-        self.url = "https://www.amazon.com.mx/s?k="
-        
-    def get_query():
-        return []
